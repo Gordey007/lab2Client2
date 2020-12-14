573455c63a41bbf33a83d3572a56604a276b133d
gordeyvasilev <gordeyvasilev@gmail.com>
2020-12-14 23:36:53 +1000

    Добавьте файлы проекта.

---
+++ lab2Client2/ClientSocket.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2Client2 {
+    class ClientSocket {
+
+        Socket sender;
+	    // Буфер для входящих данных
+	    byte[] bytes = new byte[1024];
+	    public ClientSocket(int port) {
+
+            try {
+
+	            // Соединяемся с удаленным устройством
+
+	            // Устанавливаем удаленную точку для сокета
+	            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+	            IPAddress ipAddr = ipHost.AddressList[0];
+	            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+	            sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+	            // Соединяем сокет с удаленной точкой
+	            sender.Connect(ipEndPoint);
+
+	        }
+	        catch (Exception ex) {
+	            Console.WriteLine(ex.ToString());
+	        }
+	        finally {
+	            Console.ReadLine();
+	        }
+        }
+
+        // функция отправки данных на сервер
+	    public void SendMessage(string message) {
+
+            byte[] discipline = Encoding.UTF8.GetBytes(message);
+
+	        // Отправляем данные через сокет
+	        int bytesSent = sender.Send(discipline);
+	    }
+
+        // функция получения ответа от сервера
+	    public string response()
+	    {
+	        // Получаем ответ от сервера
+	        int bytesRec = sender.Receive(bytes);
+	        string text = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+	        return text;
+	    }
+
+        // функция отключениыя
+	    public void Disconnect()
+	    {
+	        // Освобождаем сокет
+	        sender.Shutdown(SocketShutdown.Both);
+	        sender.Close();
+	    }
+	}
+}
---
+++ lab2Client2/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2Client2
+{
+
+    public partial class MainWindow : Window
+    {
+        // инициализация класса clientSocket
+        ClientSocket clientSocket;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            clientSocket = new ClientSocket(11000);
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string push = "Id: " + id.Text + ", модель: " + model.Text + ", цена: " + price.Text + ", кол-во: " + count.Text;
+            // вызов функции SendMessage из класса clientSocket и отправка введенных данных push
+            clientSocket.SendMessage(push);
+        }
+
+        private void Students_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {            
+        }
+
+
+        private void SendDiscipline_Click(object sender, RoutedEventArgs e)
+        {
+          // string strDiscipline = discipline.Text;
+         //  clientSocket.SendMessage(strDiscipline);
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // вызов функции SendMessage из класса clientSocket и отправка номер строки
+            clientSocket.SendMessage(lineNumber.Text);
+            // запись полученных данных с сервера
+            string text = clientSocket.response();
+            txtBlock.Text = text;
+        }
+
+
+        private void View_Click(object sender, RoutedEventArgs e)
+        {
+            // вызов функции SendMessage из класса clientSocket и отправка единицы
+            clientSocket.SendMessage("1");
+            // запись полученных данных с сервера
+            string text = clientSocket.response();
+            txtBlock.Text = text;
+        }
+
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            // вызов функции SendMessage из класса clientSocket и отправка двойки и текст, который нужно будет искать
+            clientSocket.SendMessage("2"+find.Text);
+            // запись полученных данных с сервера
+            string text = clientSocket.response();
+            txtBlock.Text = text;
+        }
+
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            // вызов функции SendMessage из класса clientSocket и отправка тройки и номер строки
+            clientSocket.SendMessage("3"+lineNumber.Text);
+            // запись полученных данных с сервера
+            string text = clientSocket.response();
+            txtBlock.Text = text;
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            string push = "4" + lineNumber.Text + "Id: " + id.Text + ", модель: " + model.Text + ", цена: " + price.Text + ", кол-во: " + count.Text;
+            // вызов функции SendMessage из класса clientSocket и отправка данных push на сервер
+            clientSocket.SendMessage(push);
+            // запись полученных данных с сервера
+            string text = clientSocket.response();
+            txtBlock.Text = text;         
+        }
+
+        private void LineNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
